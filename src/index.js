9b888ed7126f340b9faed70058aea5123bfd1d70
teitonny <tonnytei4@gmail.com>
2022-12-03 12:22:14 +0300

    removed linters errors

--- src/index.js
+++ src/index.js
@@ -1,68 +1,57 @@
+import './index.css';
 
 const btn = document.querySelector('.btn-click');
 const form = document.getElementById('form');
 
-const userdetaiils = document.querySelector(".table");
+const userdetaiils = document.querySelector('.table');
 
-const apikey = "MbnuI7zAKorwnEuale8c";
+const apikey = 'MbnuI7zAKorwnEuale8c';
 
-url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`;
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`;
 
 btn.addEventListener('click', () => {
-    fetch (url,
-        {
-            method: "GET"
-        }
-    )
-        .then(res => res.json())
-        .then(data => {
-            
-            // userdetaiils.innerHTML = '';
-            return data.result.forEach((el) => {
-                
-                const div = document.createElement('div');
-                const user = document.createElement('h1');
-                const score = document.createElement('h1');
-                
-                user.innerHTML = el.user,
-                
-                score.innerHTML = el.score
-                div.append(user);
-                div.append(score);
-                userdetaiils.append(div);
-            })
-          
-        })
-        
-})
+  fetch(url,
+    {
+      method: 'GET',
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      userdetaiils.innerHTML = '';
+      data.result.forEach((el) => {
+        const div = document.createElement('div');
+        const user = document.createElement('h1');
+        const score = document.createElement('h1');
 
-console.log(userdetaiils)
+        user.innerHTML = el.user;
 
-form.addEventListener('submit', async function (e){
-    e.preventDefault();
+        score.innerHTML = el.score;
+        div.append(user);
+        div.append(score);
+        userdetaiils.append(div);
+      });
+    });
+});
 
-    const formData = new FormData(form);
-    let playload = formData;
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-    console.log(' This will be empty', playload)
-    let extractedPlayload = [...playload];
-    let userValue = extractedPlayload[0][1];
-    let scoreValue = extractedPlayload[1][1];
+  const formData = new FormData(form);
+  const playload = formData;
+  const extractedPlayload = [...playload];
+  const userValue = extractedPlayload[0][1];
+  const scoreValue = extractedPlayload[1][1];
 
-    const playloadobject = {
-        user: userValue,
-        score: scoreValue,
-      };
-  
+  const playloadobject = {
+    user: userValue,
+    score: scoreValue,
+  };
 
-        await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/MbnuI7zAKorwnEuale8c/scores/`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(playloadobject),
-        })
-          .then((ele) => ele.json());
-      
-
-}) 
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/MbnuI7zAKorwnEuale8c/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(playloadobject),
+  })
+    .then((ele) => ele.json());
+});
